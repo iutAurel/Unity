d84f8da755b065400f2690b2f79dbf4c4467d1b7
Lamaracy <ahmet.alt743@gmail.com>
2022-06-23 17:25:56 +0200

    avancement parcours

--- Assets/Textures/ObstacleCoursePack/Scripts/Bounce.cs
+++ Assets/Textures/ObstacleCoursePack/Scripts/Bounce.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Bounce : MonoBehaviour
 {
-	public float force = 10f; //Force 10000f
+	public float force = 5f; //Force 10000f
 	public float stunTime = 0.5f;
 	private Vector3 hitDir;
 
