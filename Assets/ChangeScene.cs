4d1ef8ad796d9a398310572881de527664c3404e
Lamaracy <ahmet.alt743@gmail.com>
2022-06-24 13:44:51 +0200

    changement de scene

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+	public void ChangeSceneToDesert(string sceneName)
+	{
+		SceneManager.LoadScene("SceneDesert");
+	}
+	public void Exit()
+	{
+		Application.Quit();
+	}
+}
