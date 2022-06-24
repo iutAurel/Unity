bbd432822aca9708d016627491ce978cf728df28
iutAurel <aurelien.bleu@etu.univ-smb.fr>
2022-06-24 12:44:39 +0200

    player controller new code

--- Assets/Character/Scripts/PlayerController.cs
+++ Assets/Character/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,10 +10,12 @@ public class PlayerController : MonoBehaviour
     public float gravity = 20f;
     private Vector3 moveD = Vector3.zero;
     public CharacterController Cac;
+    private Animator playerAnimator;
     // Start is called before the first frame update
     void Start()
     {
         Cac = GetComponent<CharacterController>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,17 +26,39 @@ public class PlayerController : MonoBehaviour
             moveD = new Vector3(0, 0, Input.GetAxis("Vertical"));
             moveD = transform.TransformDirection(moveD);
             moveD *= speed;
+
             if (Input.GetButton("Jump"))
             {
+                playerAnimator.SetBool("JumpYes", true);
                 moveD.y = jumspeed;
-            }
-            {
 
+            }
+            else
+                playerAnimator.SetBool("JumpYes", false);
+
+
+            if (Input.GetAxis("Vertical") > 0.1)
+            {
+                playerAnimator.SetBool("SlowRunYes", true);
+            }
+            else if (Input.GetAxis("Vertical") < 0.1)
+            {
+                playerAnimator.SetBool("SlowRunYes", false);
             }
         }
-        moveD.y -= gravity * Time.deltaTime;
 
+        if (Input.GetButton("Jump"))
+        {
+            playerAnimator.SetBool("JumpYes", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("JumpYes", false);
+        }
+
+        moveD.y -= gravity * Time.deltaTime;
         transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * Time.deltaTime * speed * 10);
         Cac.Move(moveD * Time.deltaTime);
     }
 }
+
