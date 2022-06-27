b676ef799a794aa3fa6a5146f70f6ace9e797249
Ahmet ALTINEL <ahmet.alt743@gmail.com>
2022-06-27 14:12:26 +0200

    animation perso

--- Assets/Character/Scripts/PlayerController.cs
+++ Assets/Character/Scripts/PlayerController.cs
@@ -1,65 +1,84 @@
-﻿
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 6f;
 
-    public float jumspeed = 8f;
-    public float gravity = 20f;
-    private Vector3 moveD = Vector3.zero;
-    public CharacterController Cac;
+
+    [SerializeField] private float speed;
+    [SerializeField] private float jump;
+
+    private Vector3 mouvD;
+    private Vector3 velocity;
+
+    [SerializeField] private float gravity;
+
+    private CharacterController controller;
+
     private Animator playerAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
-        Cac = GetComponent<CharacterController>();
+        controller = GetComponent<CharacterController>();
         playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Cac.isGrounded)
+        Mouv();
+
+    }
+
+    private void Mouv()
+    {
+        mouvD = new Vector3(0, 0, Input.GetAxis("Vertical"));
+        mouvD = transform.TransformDirection(mouvD);
+        transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * Time.deltaTime * speed * 10);
+        mouvD *= speed;
+        if (Input.GetButton("Jump") && controller.isGrounded)
         {
-            moveD = new Vector3(0, 0, Input.GetAxis("Vertical"));
-            moveD = transform.TransformDirection(moveD);
-            moveD *= speed;
-
-            if (Input.GetButton("Jump"))
-            {
-                playerAnimator.SetBool("JumpYes", true);
-                moveD.y = jumspeed;
-
-            }
-            else
-                playerAnimator.SetBool("JumpYes", false);
-
-
-            if (Input.GetAxis("Vertical") > 0.1)
-            {
-                playerAnimator.SetBool("SlowRunYes", true);
-            }
-            else if (Input.GetAxis("Vertical") < 0.1)
-            {
-                playerAnimator.SetBool("SlowRunYes", false);
-            }
+            playerAnimator.SetBool("SlowRunYes", false);
+            playerAnimator.SetBool("JumpYes", true);
+            velocity.y = Mathf.Sqrt(jump - 2f * gravity);
         }
-
-        if (Input.GetButton("Jump"))
+        else if (controller.isGrounded == false)
         {
             playerAnimator.SetBool("JumpYes", true);
         }
+
         else
-        {
             playerAnimator.SetBool("JumpYes", false);
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            playerAnimator.SetBool("SlowRunYes", true);
+            playerAnimator.SetBool("RunBackYes", false);
         }
 
-        moveD.y -= gravity * Time.deltaTime;
-        transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * Time.deltaTime * speed * 10);
-        Cac.Move(moveD * Time.deltaTime);
+        else if (Input.GetAxis("Vertical") == 0)
+        {
+            playerAnimator.SetBool("SlowRunYes", false);
+            playerAnimator.SetBool("RunBackYes", false);
+        }
+
+        else
+        {
+            playerAnimator.SetBool("SlowRunYes", false);
+            playerAnimator.SetBool("RunBackYes", true);
+            if (Input.GetButton("Jump"))
+            {
+                playerAnimator.SetBool("JumpBackYes", true);
+            }
+            else
+                playerAnimator.SetBool("JumpBackYes", false);
+            playerAnimator.SetBool("RunBackYes", true);
+        }
+
+        controller.Move(mouvD * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
-
