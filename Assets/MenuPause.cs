460a85c40202774087b21376eb3b3cdb7b587a14
Alexc0169 <alexis.coubat@etu.univ-smb.fr>
2022-06-27 15:52:25 +0200

    mennu pause

---
+++ Assets/MenuPause.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPause : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool GamesIsPaused = false;
+    [SerializeField] GameObject pauseMenu;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamesIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                PauseMenu();
+            }
+        }
+
+        
+    }
+    public void Resume ()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GamesIsPaused = false;
+
+    }
+    public void PauseMenu()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GamesIsPaused = true;
+    }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("SceneMenu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+    }
+}
