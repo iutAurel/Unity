6c3832b29a2e455fa8599118c7e0cea0f9723aba
Lamaracy <ahmet.alt743@gmail.com>
2022-06-28 15:26:32 +0200

    respawn bureau

---
+++ Assets/Script/Re2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Re2 : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Scenebureau");
+        }
+    }
+}
+
